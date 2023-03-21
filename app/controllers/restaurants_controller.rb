abac34835bebc5a29d1b02572c8ac9765d619bb5
Leon simintei <mallfs@icloud.com>
2023-03-21 08:20:51 +0300

    controllers created

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,27 @@
 class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+
+  def index
+    render json: Restaurant.all
+  end
+
+  def show
+
+    render json: find_restaurant, serializer: RestaurantWithPizzaSerializer
+  end
+
+  def destroy
+    find_restaurant.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_restaurant
+    Restaurant.find(params[:id])
+  end
+
+  def record_not_found_response(exception)
+    render json: { error: "Restaurant not found" }, status: :not_found
+  end
 end
