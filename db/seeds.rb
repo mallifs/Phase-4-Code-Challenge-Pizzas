3d0d054bd3683ecfd3ea736b3f49bb6821bc442d
Leon simintei <mallfs@icloud.com>
2023-03-21 07:21:46 +0300

    tables and models created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "...Seeding"
+#restaurants
+restaurant1 = Restaurant.create(
+    name: "Kfc",
+    address: "cbd-123"
+)
+
+restaurant2 = Restaurant.create(
+    name: "Pizza_palace",
+    address: "uhuru-park-street"
+)
+
+restaurant3 = Restaurant.create(
+    name: "Gallitos",
+    address: "4TH_street"
+)
+
+restaurant4 = Restaurant.create(
+    name: "Lunch_Bay",
+    address: "Kalifs-street"
+)
+
+
+#pizza
+pizza1 = Pizza.create(
+    name: "NEAPOLITAN PIZZA",
+    ingredients: "Marinara sauce, Garlic, Olive oil, Basil, Oregano"
+)
+
+pizza2 = Pizza.create(
+    name: "ITALIAN PIZZA",
+    ingredients: "Tomato sauce, Garlic, Olive oil"
+)
+
+pizza3 = Pizza.create(
+    name:"AMERICAN PIZZA",
+    ingredients: "Clams, Pecorino Romano, Garlic, Olive oil, Oregano"
+)
+
+pizza4 = Pizza.create(
+    name: "AFRICAN PIZZA",
+    ingredients: "Marmalade, Goat cheese, Chicken, Hot chili peppers, Cardamon, Fennel"
+)
+
+# Create sample RestaurantPizzas
+restaurant_pizza1 = RestaurantPizza.create(price: 10, restaurant: restaurant1, pizza: pizza1)
+restaurant_pizza2 = RestaurantPizza.create(price: 12, restaurant: restaurant1, pizza: pizza2)
+restaurant_pizza3 = RestaurantPizza.create(price: 15, restaurant: restaurant2, pizza: pizza3)
+restaurant_pizza4 = RestaurantPizza.create(price: 18, restaurant: restaurant2, pizza: pizza4)
+restaurant_pizza5 = RestaurantPizza.create(price: 20, restaurant: restaurant3, pizza: pizza1)
+restaurant_pizza6 = RestaurantPizza.create(price: 22, restaurant: restaurant3, pizza: pizza3)
+
+puts "...End seeding"
